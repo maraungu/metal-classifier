bd88bdd6c5c73bf7b259a6319137934c598b4fb2
maraungu <86411069+maraungu@users.noreply.github.com>
2022-02-07 11:20:37 +0100

    started lda

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-
+import pandas as pd
+from dataprocessing import final_process
 
 if __name__ == '__main__':
-    print('PyCharm')
-
+    df = final_process()
+    # print(df[df['lyrics'].str.contains("guitars")])
+    # print(len(df))
+    df.to_csv('metal_data_final.csv', index=False, encoding='utf-8')
 
